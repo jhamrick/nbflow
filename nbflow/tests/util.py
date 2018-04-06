d071afcd2667fe7ceecf0f13f725b11a14ffb079
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-04-06 17:23:42 -0300

    Update Tests to be both Python 2 and 3 compatible
    In the particular case of test_example the json interpretation is asserted because of dicts are not always sorted the same so the assertion in Python 2 and 3 differs in the order.
    
    In all the rest Bytes and Strings fixes were done

--- nbflow/tests/util.py
+++ nbflow/tests/util.py
@@ -11,7 +11,7 @@ def run_command(cmd, retcode=0):
     p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
     code = p.wait()
     stdout, _ = p.communicate()
-    stdout = stdout.replace("\x1b[?1034h", "")
+    stdout = stdout.replace(b"\x1b[?1034h", b"")
     if code != retcode:
         print(stdout)
         raise RuntimeError("command returned unexpected code: {}".format(code))
